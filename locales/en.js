74c7e5aae8eb66b9b6922d92877aaef097354d53
Sandoche Adittane <sandoche@protonmail.com>
2020-03-07 20:15:42 +0000

    :globe_with_meridians: Text added for the homepage and navigation

--- locales/en.js
+++ locales/en.js
@@ -1,7 +1,25 @@
 module.exports = {
+  navigation: {
+    donate: 'Donate now',
+    program: 'Donation program',
+    about: 'About'
+  },
   home: {
-    header: {
-      headline: 'Hello world'
+    hero: {
+      headline: 'Join a cause and take advantage of powerful tools',
+      description:
+        'Software Donation is an open source initiative to help you to support a cause you care about with the power of software',
+      call_to_action_sentence: 'Find software for your cause'
     }
-  }
+  },
+  donation_banner: {
+    title: 'Your software can help people ?',
+    description: 'If you want to help fighting a cause, donate your software',
+    button: 'Donate now'
+  },
+  newsletter: {
+    title: 'Join the newsletter',
+    button: 'subscribe'
+  },
+  footer: {}
 }
