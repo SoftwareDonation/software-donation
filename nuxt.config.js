f09d165cafee4d0fb92978d3a2fd289a8671bc9d
Sandoche Adittane <sandoche@protonmail.com>
2020-03-07 20:20:59 +0000

    :heavy_minus_sign: Removing tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,9 +37,7 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/stylelint-module'
   ],
   /*
    ** Nuxt.js modules
