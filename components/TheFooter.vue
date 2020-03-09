adc0de44dfcde2bbeb54883d7f9daa68f5c49ff3
Sandoche Adittane <sandoche@protonmail.com>
2020-03-09 23:18:58 +0000

    :lipstick: Footer style implemented

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,13 +1,27 @@
 <template>
-  <footer class="the-footer">
-    <p>{{ $t('footer.love') }}</p>
-  </footer>
+  <base-container>
+    <footer class="the-footer">
+      <p>{{ $t('footer.love') }}</p>
+    </footer>
+  </base-container>
 </template>
 
 <script>
+import BaseContainer from './BaseContainer'
+
 export default {
-  name: 'TheFooter'
+  name: 'TheFooter',
+  components: {
+    BaseContainer
+  }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.the-footer {
+  padding: $space-lg 0;
+  border-top: 2px solid $color-grey;
+  color: $color-grey-dark;
+  font-weight: 500;
+}
+</style>
