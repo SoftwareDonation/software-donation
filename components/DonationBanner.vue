1fc98321dc715abc24f67d8cb097b73d0e7fb578
Sandoche Adittane <sandoche@protonmail.com>
2020-03-09 20:26:57 +0000

    :construction: Unstyled donation banner implemented

---
+++ components/DonationBanner.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="donation-banner">
+    <h2>{{ $t('donation_banner.title') }}</h2>
+    <p>{{ $t('donation_banner.description') }}</p>
+    <base-button :href="donateLink">{{
+      $t('donation_banner.button')
+    }}</base-button>
+  </div>
+</template>
+
+<script>
+import { DONATE_LINK } from '../config'
+import BaseButton from './BaseButton.vue'
+
+export default {
+  name: 'DonationBanner',
+  components: {
+    BaseButton
+  },
+  data() {
+    return {
+      donateLink: DONATE_LINK
+    }
+  }
+}
+</script>
--- pages/_cause/index.vue
+++ pages/_cause/index.vue
@@ -2,18 +2,21 @@
   <div>
     <HomeHeader />
     <SoftwaresList :causes="causes" :categories="categories" />
+    <DonationBanner />
   </div>
 </template>
 
 <script>
 import HomeHeader from '../../components/HomeHeader.vue'
 import SoftwaresList from '../../components/SoftwaresList.vue'
+import DonationBanner from '../../components/DonationBanner.vue'
 import causesAndCategories from '../../mixins/causesAndCategories'
 
 export default {
   components: {
     HomeHeader,
-    SoftwaresList
+    SoftwaresList,
+    DonationBanner
   },
   mixins: [causesAndCategories]
 }
