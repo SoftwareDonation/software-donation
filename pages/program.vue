077a0ee2abfe8fabcd6efc3cc8bcb0262155e3d4
Sandoche Adittane <sandoche@protonmail.com>
2020-03-10 16:37:31 +0000

    :construction: Html content added to program page

--- pages/program.vue
+++ pages/program.vue
@@ -1,19 +1,51 @@
 <template>
   <div>
     <ProgramHeader />
+    <main>
+      <section class="program__section program__section--first">
+        <div class="program__section-content program__section-content--first">
+          <h2>{{ $t('program.sections.first.title') }}</h2>
+          <p>{{ $t('program.sections.first.description') }}</p>
+          <base-button :href="donateLink" type="primary">
+            {{ $t('program.sections.first.button') }}
+          </base-button>
+        </div>
+        <div class="program__section-image program__section-image--first"></div>
+      </section>
+      <section class="program__section program__section--second">
+        <div
+          class="program__section-image program__section-image--second"
+        ></div>
+        <div class="program__section-content program__section-content--second">
+          <h2>{{ $t('program.sections.second.title') }}</h2>
+          <p>{{ $t('program.sections.second.description') }}</p>
+          <base-button :href="donateLink" type="primary">
+            {{ $t('program.sections.second.button') }}
+          </base-button>
+        </div>
+      </section>
+    </main>
     <DonationBanner />
   </div>
 </template>
 
 <script>
+import { DONATE_LINK } from '../config'
 import ProgramHeader from '../components/ProgramHeader.vue'
 import DonationBanner from '../components/DonationBanner.vue'
+import BaseButton from '../components/BaseButton.vue'
 
 export default {
   title: 'Program',
   components: {
     ProgramHeader,
-    DonationBanner
+    DonationBanner,
+    BaseButton
+  },
+  data() {
+    return {
+      donateLink: DONATE_LINK
+    }
   }
 }
 </script>
