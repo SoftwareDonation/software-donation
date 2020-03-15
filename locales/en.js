7cd98337a0158c742f3646e9151f4e00cb398a4b
Farbod Saraf <farbodsaraf@users.noreply.github.com>
2020-03-15 19:55:42 +0100

    :pencil: Update banner text for donate

--- locales/en.js
+++ locales/en.js
@@ -46,8 +46,8 @@ module.exports = {
     text: `Software Donation is an open-source foundation that aims to create a platform where software companies can join and donate their tools to empower those in need. We are currently focused on COVID-19 and accept companies in remote working and distance learning industries. Our mission is to create awareness around intangible donations like tools and help companies with their software donation strategy. We're currently accepting companies to become our founding members. If you're interested, simply reach us out at info@softwaredonation.org.`
   },
   donation_banner: {
-    title: 'Your software can help people ?',
-    description: 'If you want to help fighting a cause, donate your software',
+    title: 'Can your software make a change?',
+    description: 'Join the movement and donate your software to support a cause',
     button: 'Donate now'
   },
   newsletter: {
