08b16a30c7348321832377ed445dd62d8df5a502
Sandoche Adittane <sandoche@protonmail.com>
2020-03-16 23:54:41 +0000

    :wrench: Updating github link

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@ export const DEFAULT_CAUSE = 'work-remotely'
 export const DONATE_LINK = 'https://letsdonatesoftware.typeform.com/to/w2aXXR'
 export const CONTACT_FORM_LINK =
   'https://letsdonatesoftware.typeform.com/to/sARrWd'
-export const GITHUB_LINK = 'https://github.com/SoftwareDonation'
+export const GITHUB_LINK =
+  'https://github.com/SoftwareDonation/software-donation'
 export const MAIN_TITLE = 'Software Donation'
 export const MAIN_DESCRIPTION = 'Donate software to people in need'
 export const SOCIAL_DESCRIPTION = 'Donate software to support causes and people'
